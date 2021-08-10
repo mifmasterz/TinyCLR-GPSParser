5081a147c0e312d8bb40477d74b3025c85dfa134
Mif Masterz <mifmasterz@yahoo.com>
2021-08-10 14:59:13 +0700

    init

---
+++ src/GPS.Parser/Lib/GPS.cs
@@ -0,0 +1,334 @@
+﻿using GHIElectronics.TinyCLR.Devices.Uart;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace GPS.Parser
+{
+
+    public class GprmcParser
+    {
+        // Parse the GPRMC line
+        //
+        public static GpsPoint Parse(string line)
+        {
+            // $GPRMC,040302.663,A,3939.7,N,10506.6,W,0.27,358.86,200804,,*1A
+            //Debug.WriteLine("GpsPoint Parse");                                                                                                                                                                                                                           
+            if (!IsCheckSumGood(line))
+            {
+                return null;
+            }
+            //Debug.WriteLine(line);
+            try
+            {
+                string[] parts = line.Split(',');
+                //Debug.WriteLine(parts.Length.ToString());
+                if (parts.Length != 13)  // This GPS has extra field
+                {
+                    return null;
+                }
+                //Debug.WriteLine(parts[2]);
+                if (parts[2] != "A")
+                {
+                    return null;
+                }
+                //Debug.WriteLine(parts[9]);
+                string date = parts[9]; // UTC Date DDMMYY
+                if (date.Length != 6)
+                {
+                    return null;
+                }
+                int year = 2000 + int.Parse(date.Substring(4, 2));
+                int month = int.Parse(date.Substring(2, 2));
+                int day = int.Parse(date.Substring(0, 2));
+                string time = parts[1]; // HHMMSS.XXX
+                if (time.Length != 9)
+                {
+                    return null;
+                }
+                int hour = int.Parse(time.Substring(0, 2));
+                int minute = int.Parse(time.Substring(2, 2));
+                int second = int.Parse(time.Substring(4, 2));
+                int milliseconds = int.Parse(time.Substring(7, 2));
+                DateTime utcTime = new DateTime(year, month, day, hour, minute, second, milliseconds);
+
+                string lat = parts[3];  // HHMM.MMMM
+                if (lat.Length != 10)
+                {
+                    return null;
+                }
+                double latHours = double.Parse(lat.Substring(0, 2));
+                double latMins = double.Parse(lat.Substring(2));
+                double latitude = latHours + latMins / 60.0;
+                if (parts[4] == "S")       // N or S
+                {
+                    latitude = -latitude;
+                }
+
+                string lng = parts[5];  // HHHMM.MMMMM
+                if (lng.Length != 11)
+                {
+                    return null;
+                }
+                double lngHours = double.Parse(lng.Substring(0, 3));
+                double lngMins = double.Parse(lng.Substring(3));
+                double longitude = lngHours + lngMins / 60.0;
+                if (parts[6] == "W")
+                {
+                    longitude = -longitude;
+                }
+
+                double speed = double.Parse(parts[7]);
+                double bearing = double.Parse(parts[8]);
+
+                // Should probably validate check sum
+
+                GpsPoint gpsPoint = new GpsPoint
+                {
+                    BearingInDegrees = bearing,
+                    Latitude = latitude,
+                    Longitude = longitude,
+                    SpeedInKnots = speed,
+                    Timestamp = utcTime
+                };
+                return gpsPoint;
+
+            }
+            catch (Exception)
+            {
+                // One of our parses failed...ignore.
+                Debug.WriteLine("parse exception");
+            }
+            return null;
+        }
+
+        private static bool IsCheckSumGood(string sentence)
+        {
+            int index1 = sentence.IndexOf("$");
+            int index2 = sentence.LastIndexOf("*");
+
+            if (index1 != 0 || index2 != sentence.Length - 3)
+            {
+                return false;
+            }
+
+            string checkSumString = sentence.Substring(index2 + 1, 2);
+            int checkSum1 = Convert.ToInt32(checkSumString, 16);
+
+            string valToCheck = sentence.Substring(index1 + 1, index2 - 1);
+            char c = valToCheck[0];
+            for (int i = 1; i < valToCheck.Length; i++)
+            {
+                c ^= valToCheck[i];
+            }
+
+            return checkSum1 == c;
+        }
+    }
+
+    public static class GeoDistanceCalculator
+    {
+        private const double _earthRadiusInMiles = 3956.0;
+        private const double _earthRadiusInKilometers = 6367.0;
+        public static double DistanceInMiles(double lat1, double lng1, double lat2, double lng2)
+        {
+            return Distance(lat1, lng1, lat2, lng2, _earthRadiusInMiles);
+        }
+        public static double DistanceInKilometers(double lat1, double lng1, double lat2, double lng2)
+        {
+            return Distance(lat1, lng1, lat2, lng2, _earthRadiusInKilometers);
+        }
+        private static double Distance(double lat1, double lng1, double lat2, double lng2, double radius)
+        {
+            // Implements the Haversine formulat http://en.wikipedia.org/wiki/Haversine_formula
+            //
+            var lat = NumericExtensions.ToRadians(lat2 - lat1);
+            var lng = NumericExtensions.ToRadians(lng2 - lng1);
+            var sinLat = System.Math.Sin(0.5 * lat);
+            var sinLng = System.Math.Sin(0.5 * lng);
+            var cosLat1 = System.Math.Cos(NumericExtensions.ToRadians(lat1));
+            var cosLat2 = System.Math.Cos(NumericExtensions.ToRadians(lat2));
+            var h1 = sinLat * sinLat + cosLat1 * cosLat2 * sinLng * sinLng;
+            var h2 = System.Math.Sqrt(h1);
+            var h3 = 2 * System.Math.Asin(System.Math.Min(1, h2));
+            return radius * h3;
+        }
+    }
+
+    public static class NumericExtensions
+    {
+        public static double ToRadians(this double val)
+        {
+            return (System.Math.PI / 180) * val;
+        }
+    }
+
+    public class Reader
+    {
+        private readonly object _lock = new object();
+        private readonly UartController _serialPort;
+        private readonly int _timeOut;
+        private readonly double _minDistanceBetweenPoints;
+        private bool _isStarted;
+        private Thread _processor;
+
+        public delegate void LineProcessor(string line);
+
+        public delegate void GpsDataProcessor(GpsPoint gpsPoint);
+
+        public event LineProcessor RawLine;
+        public event GpsDataProcessor GpsData;
+
+        public bool IsStarted { get { return _isStarted; } }
+
+        public Reader(UartController serialPort)
+            : this(serialPort, 100, 0.0)
+        {
+
+        }
+        public Reader(UartController serialPort, int timeOutBetweenReadsInMilliseconds, double minDistanceInMilesBetweenPoints)
+        {
+            _serialPort = serialPort;
+            _timeOut = timeOutBetweenReadsInMilliseconds;
+            _minDistanceBetweenPoints = minDistanceInMilesBetweenPoints;
+        }
+
+        public bool Start()
+        {
+            lock (_lock)
+            {
+                if (_isStarted)
+                {
+                    return false;
+                }
+                _isStarted = true;
+                _processor = new Thread(ThreadProc);
+                _processor.Start();
+            }
+            return true;
+        }
+
+        public bool Stop()
+        {
+            lock (_lock)
+            {
+                if (!_isStarted)
+                {
+                    return false;
+                }
+                _isStarted = false;
+                if (!_processor.Join(5000))
+                {
+                    _processor.Abort();
+                }
+                return true;
+            }
+        }
+
+        private void ThreadProc()
+        {
+            Debug.WriteLine("GPS thread started...");
+            //if (!_serialPort.IsOpen)
+            //{
+            //    _serialPort.Open();
+            //}
+            while (_isStarted)
+            {
+                int bytesToRead = _serialPort.BytesToRead;
+                if (bytesToRead > 0)
+                {
+                    byte[] buffer = new byte[bytesToRead];
+                    var byteRec = _serialPort.Read(buffer, 0, buffer.Length);
+                    try
+                    {
+                        string temp = Encoding.UTF8.GetString(buffer, 0, byteRec);
+                        Debug.WriteLine(temp);
+                        ProcessBytes(temp);
+                    }
+                    catch (Exception ex)
+                    {
+                        // only process lines we can parse.
+                        Debug.WriteLine(ex.ToString());
+                    }
+                }
+
+                Thread.Sleep(_timeOut);
+            }
+            Debug.WriteLine("GPS thread stopped...");
+        }
+
+        private string _data = string.Empty;
+        private GpsPoint _lastPoint;
+        private DateTime _lastDateTime = DateTime.Now;
+
+        private void ProcessBytes(string temp)
+        {
+            while (temp.IndexOf('\n') != -1)
+            {
+                string[] parts = temp.Split('\n');
+                _data += parts[0];
+                _data = _data.Trim();
+                if (_data != string.Empty)
+                {
+                    if (_data.IndexOf("$GPRMC") == 0)
+                    {
+                        Debug.WriteLine("GOT $GPRMC LINE");
+                        if (GpsData != null)
+                        {
+                            GpsPoint gpsPoint = GprmcParser.Parse(_data);
+                            if (gpsPoint != null)
+                            {
+                                bool isOk = true;
+                                if (_lastPoint != null)
+                                {
+                                    double distance = GeoDistanceCalculator.DistanceInMiles(gpsPoint.Latitude, gpsPoint.Longitude,
+                                                                                   _lastPoint.Latitude, _lastPoint.Longitude);
+                                    double distInFeet = distance * 5280;
+                                    Debug.WriteLine("distance = " + distance + " mi (" + distInFeet + " feet)");
+                                    if (distance < _minDistanceBetweenPoints)
+                                    {
+                                        // Too close to the last point....don't raise the event
+                                        isOk = false;
+                                    }
+                                    DateTime now = DateTime.Now;
+                                    TimeSpan diffseconds = (now - _lastDateTime);
+                                    if (diffseconds.Seconds > 60)
+                                    {
+                                        // A minute has gone by, so update
+                                        isOk = true;
+                                        _lastDateTime = now;
+                                    }
+                                }
+                                _lastPoint = gpsPoint;
+
+                                // Raise the event
+                                if (isOk)
+                                {
+                                    GpsData(gpsPoint);
+                                }
+                            }
+                        }
+                    }
+                    if (RawLine != null)
+                    {
+                        RawLine(_data);
+                    }
+                }
+                temp = parts[1];
+                _data = string.Empty;
+            }
+            _data += temp;
+        }
+
+    }
+    public class GpsPoint
+    {
+        public DateTime Timestamp { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double SpeedInKnots { get; set; }
+        public double BearingInDegrees { get; set; }
+    }
+}
---
+++ src/GPS.Parser/Program.cs
@@ -0,0 +1,137 @@
+﻿using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Devices.Spi;
+using GHIElectronics.TinyCLR.Devices.Uart;
+using GHIElectronics.TinyCLR.Drivers.Sitronix.ST7735;
+using GHIElectronics.TinyCLR.Pins;
+using GPS.Parser.Properties;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+
+namespace GPS.Parser
+{
+    class Program
+    {
+        private static ST7735Controller st7735;
+        private const int SCREEN_WIDTH = 160;
+        private const int SCREEN_HEIGHT = 128;
+        static Timer timer;
+        static Random rnd;
+        static double lat, lon;
+        static GpsPoint LastPosition = null;
+        private static void Main()
+        {
+            var spi = SpiController.FromName(SC20100.SpiBus.Spi3);
+            var gpio = GpioController.GetDefault();
+
+            st7735 = new ST7735Controller(
+                spi.GetDevice(ST7735Controller.GetConnectionSettings
+                (SpiChipSelectType.Gpio, gpio.OpenPin(SC20100.GpioPin.PD10))), //CS pin.
+                gpio.OpenPin(SC20100.GpioPin.PC4), //RS pin.
+                gpio.OpenPin(SC20100.GpioPin.PE15) //RESET pin.
+            );
+
+            var backlight = gpio.OpenPin(SC20100.GpioPin.PE5);
+            backlight.SetDriveMode(GpioPinDriveMode.Output);
+            backlight.Write(GpioPinValue.High);
+
+            st7735.SetDataAccessControl(true, true, false, false); //Rotate the screen.
+            st7735.SetDrawWindow(0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1);
+            st7735.Enable();
+
+            // Create flush event
+            Graphics.OnFlushEvent += Graphics_OnFlushEvent;
+
+            // Create bitmap buffer
+            var screen = Graphics.FromImage(new Bitmap(SCREEN_WIDTH, SCREEN_HEIGHT));
+
+            //var image = Resources.GetBitmap(Resources.BitmapResources.
+            //    smallJpegBackground);
+
+            var font = Resources.GetFont(Resources.FontResources.small);
+
+            rnd = new Random();
+
+            timer = new Timer((object o) =>
+            {
+                screen.Clear();
+
+                screen.FillEllipse(new SolidBrush(System.Drawing.Color.FromArgb
+                    (255, 255, 0, 0)), 0, 0, 80, 64);
+
+                screen.FillEllipse(new SolidBrush(System.Drawing.Color.FromArgb
+                    (255, 0, 0, 255)), 80, 0, 80, 64);
+
+                screen.FillEllipse(new SolidBrush(System.Drawing.Color.FromArgb
+                    (128, 0, 255, 0)), 40, 0, 80, 64);
+                /*
+                //screen.DrawImage(image, 56, 50);
+
+                screen.DrawRectangle(new Pen(Color.Yellow), 10, 80, 40, 25);
+                screen.DrawEllipse(new Pen(Color.Purple), 60, 80, 40, 25);
+                screen.FillRectangle(new SolidBrush(Color.Teal), 110, 80, 40, 25);
+
+                screen.DrawLine(new Pen(Color.White), 10, 127, 150, 127);
+                screen.SetPixel(80, 92, Color.White);
+                */
+                if (LastPosition != null)
+                {
+                    screen.DrawString("[GPS PARSER]", font, new SolidBrush(Color.Green), 10, 80);
+                    screen.DrawString($"lat = {string.Format("{0:n4}", LastPosition.Latitude)}, lon = {string.Format("{0:n4}", LastPosition.Longitude)}", font, new SolidBrush(Color.Green), 10, 100);
+                }
+                screen.Flush();
+            }, null, 1000, 1000);
+
+            StartGPS();
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void Graphics_OnFlushEvent(Graphics sender, byte[] data, int x, int y, int width, int height, int originalWidth)
+        {
+            st7735.DrawBuffer(data);
+        }
+
+        public static void StartGPS()
+        {
+            //using NEO 6M GPS MODULE
+            UartController serialPort = UartController.FromName(SC20100.UartPort.Uart1);
+            var uartSetting = new UartSetting()
+            {
+                BaudRate = 9600,
+                DataBits = 8,
+                Parity = UartParity.None,
+                StopBits = UartStopBitCount.One,
+                Handshaking = UartHandshake.None,
+            };
+            serialPort.SetActiveSettings(uartSetting);
+            serialPort.Enable();
+
+            Reader gpsShield = new Reader(serialPort, 100, 0.0);
+            gpsShield.GpsData += GpsShield_GpsData;
+            gpsShield.Start();
+        }
+
+        static void WriteLine(string Message, bool Status = false)
+        {
+            Debug.WriteLine(Message);
+        }
+
+        private static void GpsShield_GpsData(GpsPoint gpsPoint)
+        {
+            Debug.WriteLine("time: " + gpsPoint.Timestamp + "Lat/Lng: " + gpsPoint.Latitude + "/" + gpsPoint.Longitude);
+            //Lcd.Clear();
+            WriteLine("  GPS MONITOR ", false);
+            WriteLine("              ", false);
+            WriteLine("Time: " + gpsPoint.Timestamp + " UTC     ");
+            //string latStr = gpsPoint.Latitude.ToString("F4");
+            //string lonStr = gpsPoint.Longitude.ToString("F4");           
+            LastPosition = gpsPoint;
+            WriteLine("Lat: " + lat + "     ");
+            WriteLine("Lng: " + lon + "     ");
+        }
+    }
+
+}
